4df5c33fa261ea409aaf39b859cafd7db636023b
Akiyoshi Tsuchida <tsuchiaki2012@gmail.com>
2017-10-31 07:46:37 +0900

    整形　改行削除

--- AWUSetting.ps1
+++ AWUSetting.ps1
@@ -1,11 +1,6 @@
 # PowerShellの拡張
 Install-PackageProvider -Name NuGet -Force;
-
 # Windows Update用のPowerShellモジュール
 Install-Module PSWindowsUpdate;
-
 # Microsoft Update クライアントOS用 
-# Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d;
-
-# Windows Update Server OS用
-Add-WUServiceManager -ServiceID 9482f4b4-e343-43b6-b170-9a65bc822c77;
+Add-WUServiceManager -ServiceID 7971f918-a847-4430-9279-4a52d1efe18d;
